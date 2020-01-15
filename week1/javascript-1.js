275550bf3c8cf5e599477c68398a2101550f7233
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-01-15 16:35:15 -0700

    Added js file

---
+++ week1/javascript-1.js
@@ -0,0 +1,46 @@
+//JAVASCRIPT 1 REVIEW
+//Many of these problems will require you to console log certain values.  To do this we use:
+console.log('A value')
+//Click run to test your values.
+
+//Data types
+//What are our known data types?
+/*
+TYPE YOUR ANSWERS HERE
+
+
+*/
+
+//Variables
+//Declare a variable called name and give it a value of your name as a string.  Log it to the console.
+
+//Create an array called nums which will hold the numbers 1 - 5
+
+//Create an object called person, this object should have 3 key value pairs: name, which should be your name, age which should be your age, and hobbies which should be an array of 3 strings, each a hobby you enjoy.  Console log the first item in this array.
+//HINT: You will need to use both dot and bracket notation to access this item
+
+//Reassigning Variables
+let instructor = 'Billy Bob'
+//Reassign the instrutor variable to have a value of your instructors name
+
+//If statements
+let age = 15
+//Write an if statement that checks the value of the age variable.
+//If age is greater than 16, console log 'You can drive' otherwise console log 'You cannot drive'
+
+//Write an if statement that checks the following:
+//If age is between 0 and 12, console log 'You could still go to hogwarts'
+//If age is between 13 and 17 console log 'You probably won't go to hogwarts'
+//If age is 18 or greater console log 'Hogwarts is right out'
+
+//Write an if statement that checks if age is equal to 16 or 21.  If it is, console log 'what a big year' otherwise console log 'no big deal'
+
+//Try changing the value of the age variable to get different results from your if statements
+
+//Functions
+//Write a function called sum that takes two parameters.  Both of these should be numbers.  The function should return the sum of the two numbers
+
+//Write a function called whatAmI which takes two parameters, a name and a job.
+//It should return a string of 'Yer a job, name'
+//For example, passing 'Harry' and 'wizard' as arguments would return 'Yer a wizard, Harry'  Invoke the function passing in different values to test it.
+//HINT: Try using template strings on this one
